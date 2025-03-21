a39005316fc86be7631725eba208f837306ecf00
SofyaRusyaeva <sofyarusyaeva@gmail.com>
2025-03-21 14:57:58 +0400

    password hash

---
+++ src/main/resources/db/migration/V2__insert-values.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (user_name, email, password, phone) VALUES ('Ann', 'ann@gmail.com', '123', '1234567890');
+INSERT INTO users (user_name, email, password, phone) VALUES ('Milana', 'milana@gmail.com', '456', '0987654321');
+
+INSERT INTO brand (name, country) VALUES ('Clinique', 'France');
+INSERT INTO brand (name, country) VALUES ('Loreal', 'France');
+
+INSERT INTO product (name, description, category, brand_id, price) VALUES ('lipstick', 'matt', 'Category1', 1, 1999.99);
+INSERT INTO product (name, description, category, brand_id, price) VALUES ('cream', 'hand cream', 'Category2', 2, 299.99);
