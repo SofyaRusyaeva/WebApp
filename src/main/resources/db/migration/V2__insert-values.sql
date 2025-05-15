d32ab52733da7d60e7e57a0155c491251d088f54
Валерия <zhdanova.valeria.ru@mail.ru>
2025-05-15 17:37:42 +0400

    add products

--- src/main/resources/db/migration/V2__insert-values.sql
+++ src/main/resources/db/migration/V2__insert-values.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (user_name, email, password, phone) VALUES ('Ann', 'ann@gmail.com', '$2a$10$9teQIvuui0beXwbF/471he21dNZWXBvDLtJtTpF4QRQITWW2LWQAG', '1234567890');
-INSERT INTO users (user_name, email, password, phone) VALUES ('Milana', 'milana@gmail.com', '$2a$10$/5XG65.Q2XiBt7NgJxMuV.4KQ.uANMtWzpifAcKJf0Wuq8RuZZhJG', '0987654321');
+INSERT INTO users (user_name, email, password, phone) VALUES ('Ann', 'ann@gmail.com', '$2a$10$9teQIvuui0beXwbF/471he21dNZWXBvDLtJtTpF4QRQITWW2LWQAG', '+71111111111');
+INSERT INTO users (user_name, email, password, phone) VALUES ('Milana', 'milana@gmail.com', '$2a$10$/5XG65.Q2XiBt7NgJxMuV.4KQ.uANMtWzpifAcKJf0Wuq8RuZZhJG', '+72222222222');
 
 INSERT INTO brand (name, country) VALUES ('Clinique', 'France');
 INSERT INTO brand (name, country) VALUES ('Loreal', 'France');
@@ -7,6 +7,8 @@ INSERT INTO brand (name, country) VALUES ('Art Visage', 'Russia');
 INSERT INTO brand (name, country) VALUES ('Holika holika', 'Korea');
 INSERT INTO brand (name, country) VALUES ('Tom Ford', 'USA');
 INSERT INTO brand (name, country) VALUES ('Rad', 'USA');
+INSERT INTO brand (name, country) VALUES ('Nivea', 'Italy');
+INSERT INTO brand (name, country) VALUES ('Organic shop', 'Russia');
 
 INSERT INTO product (name, description, category, brand_id, price) VALUES ('Almost lipstick', 'Black Honey', 'makeup', 1, 2000);
 INSERT INTO product (name, description, category, brand_id, price) VALUES ('Vitamino Color', 'Shampoo for colored hair, 300 мл', 'haircare', 2, 1170);
@@ -16,6 +18,11 @@ INSERT INTO product (name, description, category, brand_id, price) VALUES ('Wate
 INSERT INTO product (name, description, category, brand_id, price) VALUES ('Lost cherry', 'Sweetness. The temptation. 50 мл', 'perfume', 5, 31500);
 INSERT INTO product (name, description, category, brand_id, price) VALUES ('Miracle Touch', 'Оттенок 101, золотисто-бежевый', 'makeup', 3, 230);
 INSERT INTO product (name, description, category, brand_id, price) VALUES ('Eye Eye Eye! Vinyl Eyeliner', 'Оттенок 004, Acid blue', 'makeup', 6, 450);
+INSERT INTO product (name, description, category, brand_id, price) VALUES ('Cleansing Balm', 'Take The Day Off Cleansing Balm, 125 мл', 'skincare', 1, 2700);
+INSERT INTO product (name, description, category, brand_id, price) VALUES ('KOSMA white plastic base', 'Набор пилок для ногтей, 5 шт', 'nail_care', 3, 150);
+INSERT INTO product (name, description, category, brand_id, price) VALUES ('NIVEA Men', 'Крем для лица мужской интенсивно увлажняющий', 'men_cosmetics', 7, 235);
+INSERT INTO product (name, description, category, brand_id, price) VALUES ('Sweet Vanilla', 'Гель для душа, 500 мл', 'organic', 8, 200);
+INSERT INTO product (name, description, category, brand_id, price) VALUES ('#3 by Sergey Turchaninov', 'Кисть круглая крупная', 'other', 3, 1600);
 
 INSERT INTO cart (total_price, user_id) VALUES (0, 1);
 INSERT INTO cart (total_price, user_id) VALUES (0, 2);
